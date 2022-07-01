a9911417f2e5098a8faad47ef8261b6ff01498b7
Jarek <j.szczepaniak@albedomarketing.pl>
2022-07-01 12:49:49 +0200

    Feat: add base input component + input error component.

---
+++ src/components/modules/form/BaseInputComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="input">
+    <label :class="{error: errors.length}"
+           :for="id">
+      {{ label }}</label>
+    <input
+      :type="type"
+      :id="id"
+      :value="value"
+      :placeholder="placeholder"
+      :class="{error: errors.length}"
+      :maxlength="maxLength"
+      @input="formatValue($event)"
+    >
+    <input-errors-component
+      :errors="errors"
+    ></input-errors-component>
+  </div>
+</template>
+
+<script>
+import InputErrorsComponent from "@/components/modules/form/InputErrorsComponent"
+export default {
+  name: "BaseInputComponent",
+  components: {
+    InputErrorsComponent
+  },
+  props: {
+    id: String,
+    label: String,
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: String,
+    placeholder: String,
+    maxLength: String,
+    errors: {
+      type: Array,
+      default: () => []
+    },
+    format: String
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    formatValue ($event) {
+      this.$emit('input', $event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
