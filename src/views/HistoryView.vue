3aab0c608c6e222c2c901fe237cbd3c3f3b1f7b3
Jarek <j.szczepaniak@albedomarketing.pl>
2022-07-01 15:35:48 +0200

    Feat: add history.

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="container">
     <div class="history">
-      <p>history</p>
+      <h2 class="text-center mb-5">History songs</h2>
+      <div class="history__list-container">
+        <ul class="history__list">
+          <li class="history__list-item" v-for="(song, i) in history" :key="i">
+            <p>{{ i + 1 }}.</p>
+            <p>{{ song.name }}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 
@@ -9,7 +17,16 @@
 
 <script>
 export default {
-  name: "HistoryView"
+  name: "HistoryView",
+  data () {
+    return {
+      history: [
+        { name: 'Eminem just lose it' },
+        { name: 'Mad world' },
+        { name: 'America' }
+      ]
+    }
+  }
 }
 </script>
 
@@ -20,6 +37,36 @@ export default {
     @media screen and (max-width: 767px) {
       margin-top: 15px;
       margin-right: 60px;
+    }
+
+    &__list-container {
+      max-width: 600px;
+      background-color: #fff;
+      margin: 0 auto;
+      border-radius: 10px;
+      box-shadow: 2px 2px 3px #000;
+    }
+
+    &__list {
+      padding: 0;
+      list-style-type: none;
+    }
+
+    &__list-item {
+      padding: 15px 20px;
+      display: flex;
+      justify-content: space-between;
+      font-weight: 700;
+
+      &:nth-child(even) {
+        background-color: $color-third;
+        color: #fff;
+      }
+
+      p {
+        font-size: 2rem;
+        margin-bottom: 0;
+      }
     }
   }
 </style>
