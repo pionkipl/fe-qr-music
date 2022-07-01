b982a8750e3b3e1d59af921312b609e1c59f44d1
Jarek <j.szczepaniak@albedomarketing.pl>
2022-07-01 15:18:09 +0200

    Feat: modify axios helper.

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,26 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '/api',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
-api.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-api.interceptors.request.use(
-    config => {
-        const token = sessionStorage.getItem("access_token");
-
-        if (token) {
-            config.headers.common["Authorization"] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    }
-);
-
-export default api;
+export default api
