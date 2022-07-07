01046a89353bfbe2139aecd77d3b895083af9668
Jarek <j.szczepaniak@albedomarketing.pl>
2022-07-07 21:50:24 +0200

    Feat: add form response.

---
+++ src/components/modules/form/FormResponseComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    v-if="JSON.stringify(response) !== '{}'"
+    class="fw700 text-center mt-4"
+  >
+    <template v-if="response.status === 500">
+      <p class="mb-0 resp err">Something went wrong, try again later.</p>
+    </template>
+    <template v-if="response.status === 200">
+      <p class="mb-0 resp">{{ message }}</p>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormResponseComponent",
+  props: {
+    response: Object
+  },
+  computed: {
+    message: function () {
+      try {
+        return this.response.data.message || ''
+      } catch {
+        return ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .resp {
+    background-color: #32B4FD;
+    padding: 15px 30px;
+    border-radius: 15px;
+    color: #fff;
+
+    &.err {
+        background-color: #A74FA2;
+     }
+  }
+</style>
